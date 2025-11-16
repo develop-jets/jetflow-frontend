a210ea1e7bd16f40b61ae734997797fa58eb20b2
Yash Gaur <developer_yash_gaur@Yashs-MacBook-Pro.local>
2025-11-16 20:40:13 +0530

    changed

--- jet-flow-frontend/src/app/(private)/app/page.tsx
+++ jet-flow-frontend/src/app/(private)/app/page.tsx
@@ -18,6 +18,7 @@ import {
   Legend
 } from 'chart.js';
 
+
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 export default function AppHomePage() {
